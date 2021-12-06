162587035a5385189f3fff0ce99f2e59cbd64185
Oleg Subotin <subotinoleg0@gmail.com>
2021-12-06 22:35:13 +0200

    copied

---
+++ js/task-01.js
@@ -0,0 +1,5 @@
+const navEl = document.querySelectorAll('.item');
+console.log('Number of categories: ', navEl.length);
+//==========
+const navElTitle = document.querySelectorAll('.item');
+console.log(navElTitle.children);
