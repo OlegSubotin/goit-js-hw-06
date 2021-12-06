819600d32f530a11f0ca0ab9ae96f1181541b43f
Oleg Subotin <subotinoleg0@gmail.com>
2021-12-07 00:57:26 +0200

    forth task done

---
+++ js/task-04.js
@@ -0,0 +1,21 @@
+const button = {
+    add: document.querySelector("[data-action='increment']"),
+    sub: document.querySelector("[data-action='decrement']"),
+    span: document.querySelector("#value"),
+  };
+
+let counterValue = 0;
+const increment = () => {
+  counterValue += 1;
+
+ button.span.textContent = counterValue;
+};
+
+const decrement = () => {
+  counterValue -= 1;
+
+  button.span.textContent = counterValue;
+};
+
+button.add.addEventListener("click", increment);
+button.sub.addEventListener("click", decrement);
