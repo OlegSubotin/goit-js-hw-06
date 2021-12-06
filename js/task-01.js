becebe0fcf0d70cdd7caf1708639a4b40b76b43c
Oleg Subotin <subotinoleg0@gmail.com>
2021-12-06 23:40:58 +0200

    first task done

--- js/task-01.js
+++ js/task-01.js
@@ -1,5 +1,11 @@
-const navEl = document.querySelectorAll('.item');
-console.log('Number of categories: ', navEl.length);
-//==========
-const navElTitle = document.querySelectorAll('.item');
-console.log(navElTitle.children);
+const categoryQty = document.querySelectorAll('.item');
+console.log('Number of categories: ', categoryQty.length);
+//=========
+
+categoryQty.forEach(category =>{
+    const getCategoryTitle = category.querySelector('h2');
+    console.log('Category: ', getCategoryTitle.textContent);
+
+    const getCategoryQnt = category.querySelectorAll('li');
+    console.log('Elements: ', getCategoryQnt.length);
+});
