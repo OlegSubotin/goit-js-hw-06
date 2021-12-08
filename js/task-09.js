e8f66d76a9f614d58fb5ac5e1fbec5cd46e3f98f
Oleg Subotin <subotinoleg0@gmail.com>
2021-12-08 13:30:18 +0200

    task 9 done

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+  const spanItem = document.querySelector('.color');
+  const btnItem = document.querySelector('.change-color');
+
+
+  btnItem.addEventListener('click', changeColor);
+
+function changeColor () {
+  document.body.style.backgroundColor = getRandomHexColor();
+  spanItem.textContent = getRandomHexColor();
+}
