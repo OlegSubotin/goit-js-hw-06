8878fbacab1bf5803e66e51120f1161304fe0a67
Oleg Subotin <subotinoleg0@gmail.com>
2021-12-08 12:58:23 +0200

    task eighth done

--- js/task-08.js
+++ js/task-08.js
@@ -1,1 +1,20 @@
+const form = document.querySelector('.login-form');
+
+
+form.addEventListener('submit', formSubmit);
+function formSubmit (evt){
+    evt.preventDefault();
+
+    let formItems = evt.currentTarget.elements
+    let email = formItems.email.value;
+    let password = formItems.password.value;
+
+    if (email==='' || password ===''){
+        return alert('все поля должны быть заполнены');
+    }else{
+    console.log('email:', email);
+    console.log('password:', password);
+    form.reset();
+    }
+}
 
